2dbecd7964ff82bb4dcb9507c1fbb78eb002147c
Ludovic Ollagnier <ludovic@ollagnier.me>
2019-03-12 09:31:04 +0100

    first documents

---
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+
+
+
+let languageName = "Swift"
+var version = 4.22575764653734736536
+var introductionYear: UInt = 2014
+var isAwesome = true
+
+let test: String
+
+if true {
+    test = "true"
+} else {
+    test = "false"
+}
+
+let a = 5
+let b = 2
+
+let c = Double(a) / Float64(b)
+
+let annonce = languageName + " was introduced in \(introductionYear). It is in version \(version.binade)"
+
+String(version)
---
+++ Projets/HC/HC/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HC
+//
+//  Created by Ludovic Ollagnier on 11/03/2019.
+//  Copyright © 2019 Human Coders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+    }
+
+
+}
+
+
